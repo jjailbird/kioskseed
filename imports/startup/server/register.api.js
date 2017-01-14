a661731b338b578ac55e5adc1f3f0026ae1934e0
jailbird <jjailbird@gmail.com>
2017-01-14 15:17:54 +0900

    add MONGO_URL

--- imports/startup/server/register.api.js
+++ imports/startup/server/register.api.js
@@ -47,6 +47,10 @@ console.log('imageHost', imageHost);
 Meteor.startup(() => {
   // process.env.MAIL_URL = 'smtp://postmaster%40meteorize.mailgun.org:YOURPASSWORD@smtp.mailgun.org:587';
   process.env.ROOT_URL = rootUrl;
+  if (Meteor.settings.public.mongoURL) {
+    process.env.MONGO_URL = Meteor.settings.public.mongoURL;
+    console.log('MONGO_URL', Meteor.settings.public.mongoURL);
+  }
   var childProcess = require('child_process');
   childProcess.exec(`start chrome --kiosk ${rootUrl}`);
 });
